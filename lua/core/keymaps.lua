cb47db347bad92511b4409cf410289bf845894d9
Shubhadeep Ghosh <75712707+sdpghosh@users.noreply.github.com>
2025-04-20 12:57:31 +0530

    Add Hex dum Keybindings in keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pk", vim.cmd.Ex)
+
+-- === Hex (xxd) Keybindings ===
+
+vim.keymap.set("n", "<leader>hx", function() vim.cmd("%!xxd") end)           -- Hex dump current buffer
+vim.keymap.set("n", "<leader>hr", function() vim.cmd("%!xxd -r") end)        -- Reverse hex dump to normal text
+
